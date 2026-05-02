1d986753f84f8dbe507ce4d3eaecd7090b75b324
Bernabe <bernabeferreyra78@gmail.com>
2026-05-01 23:57:09 -0300

    Add: ProductHelper1 y ProductHelper2.
    Add: Partial class ProductHelper

---
+++ Dsw2026Ej8/ProductHelper1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal partial class ProductHelper
+    {
+        public string ObtenerEtiquetaProducto(long code, string description, decimal price) => $"[{code} {description} - {GetPriceWithFormat(price)}";
+    }
+    
+}
