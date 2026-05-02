f7629861a2e1999c0ecf0c97db7a3351d155dc4a
Bernabe <bernabeferreyra78@gmail.com>
2026-05-02 00:23:55 -0300

    Add: Clase Problema3 con la implementación del método CompararCopias.

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema3
+    {
+        public string CompararCopias(int originalValue, Product product)
+        {
+            int valueCopy = originalValue;
+            valueCopy++;
+            Product productCopy = product;
+            productCopy.SetDescription("Nueva descripción");
+
+            return $"{originalValue}-{valueCopy}-{productCopy.GetDescription()}";
+        }
+    }
+    internal class Product 
+    {
+        private string _description;
+
+        public string GetDescription() => _description;
+        public string SetDescription(string description) => _description = description;
+    }
+}
